7132b98d60dbc371fd28197a62d134eea11ba6fc
Paul-Blackham <paulsblackham@sky.com>
2016-09-12 15:54:18 +0100

    Added new test scene
    added new test scene due to instability in visualisation scene. Added
    clouds and got multi-display working ok.

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+    public float moveSpeed;
+
+	// Use this for initialization
+	void Start () {
+        //print("yo im a player");
+        moveSpeed = 6f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //print(Input.GetAxis("Horizontal"));
+
+        transform.Translate(moveSpeed * Input.GetAxis("Horizontal") * Time.deltaTime, 0f, 0f);
+        transform.Translate(0f, 0f, moveSpeed * Input.GetAxis("Vertical") * Time.deltaTime);
+
+    }
+}
