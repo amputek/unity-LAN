e246a9787b3afbb00bbf930d84d70a6996bbc570
Paul-Blackham <paulsblackham@sky.com>
2016-09-12 18:21:53 +0100

    changed to dark scene
    added chopper and spotlight and added script (unfinished) to create
    circling animation

---
+++ Assets/Scripts/ChopperMovement.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChopperMovement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    // put general rotation here
+	}
+}
