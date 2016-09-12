9051ba7bb4d216019ac5cb7e67ced24c9a4e9f5e
Rob Dawson <r.dawson.7291@googlemail.com>
2016-09-12 20:40:00 +0100

    Environmental Collisions

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,17 +3,27 @@ using UnityEngine.Networking;
 
 public class PlayerController : MonoBehaviour
 {
+
+	public CharacterController controller;
+
 	void Start(){
 
 	}
 
 	void Update()
 	{
-		var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+		var x = Input.GetAxis("Horizontal") * Time.deltaTime * 3.0f;
 		var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
 
-		transform.Rotate(0, x, 0);
-		transform.Translate(0, 0, z);
+		//transform.Rotate(0, x, 0);
+	//	transform.Translate(x, 0, z);
+
+		Vector3 mvt = new Vector3 (x, 0, z);
+
+		mvt = transform.TransformDirection(mvt);
+
+		controller.Move (mvt);
+
 	}
 
 
