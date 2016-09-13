062b5264196bfc7f7f5bbef15ab4626c62d7dcc5
Paul-Blackham <paulsblackham@sky.com>
2016-09-13 10:30:51 +0100

    Added helicopter movement

--- Assets/Scripts/ChopperMovement.cs
+++ Assets/Scripts/ChopperMovement.cs
@@ -10,6 +10,7 @@ public class ChopperMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	    // put general rotation here
-	}
+        // put general rotation here
+        transform.Rotate(0, 0.1f, 0);
+    }
 }
