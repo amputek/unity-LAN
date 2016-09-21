dcf05c09c5480b430f2e728669a22f8f7012a1d7
Paul <paulsblackham@sky.com>
2016-09-21 14:03:10 +0100

    adding bit by bit

---
+++ Assets/Scripts/DayNightCylcleIR.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DayNightCylcleIR : MonoBehaviour {
+
+    public Light light;
+
+    // Use this for initialization
+    void Start () {
+        light = GetComponent<Light>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKey(KeyCode.P))
+        {
+            transform.Rotate(10f * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.L))
+        {
+            transform.Rotate(-10f * Time.deltaTime, 0, 0);
+        }
+
+        LightIntensity();
+
+    }
+
+    void LightIntensity ()
+    {
+        // Use Lerp
+        if (transform.rotation.eulerAngles.x > 200 & transform.rotation.eulerAngles.x < 340)
+        {
+            light.intensity = Mathf.Lerp(light.intensity, 0.05f, Time.deltaTime * 0.3f);
+        }
+        if (transform.rotation.eulerAngles.x > 200 & transform.rotation.eulerAngles.x > 340)
+        {
+            light.intensity = Mathf.Lerp(light.intensity, 5f, Time.deltaTime * 0.3f);
+        }
+    }
+}
---
+++ Assets/Scripts/MoonCylcle.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoonCylcle : MonoBehaviour {
+    public float distance = -10000.0f;
+    public float scale = 500.0f;
+	// Use this for initialization
+	void Start () {
+        transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y, distance);
+        transform.localScale = new Vector3(scale, scale, scale);
+	}
+
+	void Update () {
+        
+    }
+}
