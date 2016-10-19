4578579aad62237f0150fa547cc5018d1865e103
Paul-Blackham <paulsblackham@sky.com>
2016-10-19 16:58:34 +0100

    tested resolution

---
+++ Assets/Scripts/ScreenResolutionManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScreenResolutionManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Screen.SetResolution(2560, 3860, false);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
